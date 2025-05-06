711d5b69fda82fca24cec6589d8878cd146ebdbd
Дмитрий Вислобоков <dmitryvislobokov@MacBook-Air-Dmitrij.local>
2025-05-06 23:26:36 +0300

    add module utils, and add pass_util util

---
+++ src/backend/utils/pass_util.py
@@ -0,0 +1,25 @@
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+SECRET_KEY = ""
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTS = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTS)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        key=SECRET_KEY,
+        algorithm=ALGORITHM)
+    return encoded_jwt
